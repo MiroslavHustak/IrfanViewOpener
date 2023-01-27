6008b23b9afd0891a88cd86c2f67788c338964ba
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2023-01-27 21:15:28 +0100

    27-01-2023

--- IrfanViewOpener/MyTypes/Strings.fs
+++ IrfanViewOpener/MyTypes/Strings.fs
@@ -2,14 +2,12 @@
 
 open System
 
-let private myOption s = 
+let (|StringNonN|) s = 
     s 
     |> Option.ofObj 
     |> function 
         | Some value -> string value
         | None       -> String.Empty
-
-let (|StringNonN|) s = myOption s 
 
 //let private myString (StringNonN s) = s //to be implemented in the relevant module
 
